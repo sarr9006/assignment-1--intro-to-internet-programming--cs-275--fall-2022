990d9052022c39cadbf57cbc62bc5a8fea8875cd
SamStigs <102331245+SamStigs@users.noreply.github.com>
2022-10-06 20:31:44 -0400

    Coded to add array from output

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,3 +1,28 @@
 window.onload = () => {
-    alert(`JavaScript Works`);
+
+    let output = document.getElementById(`output`);
+    let content = ``;
+    let counter = 1;
+
+    let input = window.prompt(`Please Enter a whole number for your Matrix`,`No Negatives, no letters please.`);
+    input = parseInt(input, 10);
+    console.log(typeof input);
+    let myNewArray = new Array(input);
+    console.log(`The size of the array is ${myNewArray.length}`);
+
+    content = `<table>`;
+
+    for(let i = 0; i < myNewArray.length; i++) {
+        content += `<tr>`;
+
+        for(let j = 0; j < myNewArray.length; j++) {
+            content += `<td>${counter++}</td>`;
+        }
+
+        content += `</tr>`;
+    }
+
+    content += `</table>`;
+
+    output.innerHTML = content;
 };
